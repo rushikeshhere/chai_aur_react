8ddd124e03fc4ee2b6fef830f5073e01c82e7f6c
Rushikesh Here <rushikeshhere@gmail.com>
2024-07-01 14:34:02 +0530

    counter project

---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const [isButtonDisabled, setButtonDisabled] = useState(false);
+
+  const disableButton = () => {
+    setButtonDisabled(true);
+    alert("You will not allowed to increase more than 20!");
+  }
+
+  const addValue = () => {
+    if (count == 20) {
+      disableButton();
+    }
+    else {
+      setCount(count + 1)
+    }
+  }
+  const removeCount = () => {
+    if (count === 0) {
+      setCount(count)
+    } else {
+      setCount(count - 1)
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai Aur React</h1>
+      <h3>Counter value is :{count}</h3>
+      <button onClick={addValue}>Add count</button>
+      <button onClick={removeCount}>Remove Count</button>
+    </>
+  )
+}
+
+export default App
