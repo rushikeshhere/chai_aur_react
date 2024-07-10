ff0346db03a8f8f4cfb24a8ac28144a55baf41ee
Rushikesh Here <rushikeshhere@gmail.com>
2024-07-10 19:01:33 +0530

    imported Input.jsx

--- 06_CurrencyConverter/src/App.jsx
+++ 06_CurrencyConverter/src/App.jsx
@@ -1,14 +1,61 @@
 import { useState } from 'react';
 import './App.css'
+import { Input } from "./components"
+import useCurrencyInfo from './hooks/useCurrencyInfo';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+  const options = Object.keys(currencyInfo);
 
   return (
-    <>
-      <h1 className='text-3xl bg-orange-500 rounded-lg py-3 '>Currency Converter App with chai</h1>
-    </>
-  )
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url('${BackgroundImage}')`,
+      }}
+    >
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+
+            }}
+          >
+            <div className="w-full mb-1">
+              <InputBox
+                label="From"
+
+              />
+            </div>
+            <div className="relative w-full h-0.5">
+              <button
+                type="button"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+
+              >
+                swap
+              </button>
+            </div>
+            <div className="w-full mt-1 mb-4">
+              <InputBox
+                label="To"
+
+              />
+            </div>
+            <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+              Convert
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App
