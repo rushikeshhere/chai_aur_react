a90cc16b67c987a0a04e50fa5595fb98c4fe792a
Rushikesh Here <rushikeshhere@gmail.com>
2024-07-11 14:39:45 +0530

    added multiple functionality

--- 06_CurrencyConverter/src/App.jsx
+++ 06_CurrencyConverter/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
-        backgroundImage: `url('${BackgroundImage}')`,
+        backgroundImage: `url('https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
       }}
     >
       <div className="w-full">
@@ -35,12 +35,18 @@ function App() {
           <form
             onSubmit={(e) => {
               e.preventDefault();
+              convert();
 
             }}
           >
             <div className="w-full mb-1">
-              <InputBox
+              <Input
                 label="From"
+                amount={amount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => { setAmount(amount) }}
+                selectCurrency={from}
+                onAmountChange={(amount) => setAmount(amount)}
 
               />
             </div>
@@ -48,19 +54,25 @@ function App() {
               <button
                 type="button"
                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
-
+                onClick={swap}
               >
                 swap
               </button>
             </div>
             <div className="w-full mt-1 mb-4">
-              <InputBox
+              <Input
                 label="To"
+                amount={convertedAmount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => { setTo(currency) }}
+                selectCurrency={to}
+                amountDisable
 
               />
             </div>
             <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
-              Convert
+              Convert {from.toUpperCase()} to {to.toUpperCase
+                ()}
             </button>
           </form>
         </div>
