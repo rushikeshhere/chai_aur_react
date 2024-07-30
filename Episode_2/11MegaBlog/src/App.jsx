b8f64f55a702afc9c53d229451d7f961e061f236
Rushikesh Here <rushikeshhere@gmail.com>
2024-07-30 18:44:28 +0530

    Imported env Variable in App.jsx

--- Episode_2/11MegaBlog/src/App.jsx
+++ Episode_2/11MegaBlog/src/App.jsx
@@ -5,9 +5,10 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
+
       <h1>Learn code with me</h1>
     </>
   )
