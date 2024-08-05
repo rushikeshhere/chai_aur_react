767eb738c19a9504645f23e22687d1950f88e3e6
Rushikesh Here <rushikeshhere@gmail.com>
2024-08-05 23:21:17 +0530

    added imports

--- Episode_2/11MegaBlog/src/components/index.js
+++ Episode_2/11MegaBlog/src/components/index.js
@@ -1,7 +1,12 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    LogoutBtn,
+    Logo
 }
