c17c40822769032750041dc66f7e3bcad545c0a6
Rushikesh Here <rushikeshhere@gmail.com>
2024-08-05 23:22:23 +0530

    used useSelector and useNavigate

--- Episode_2/11MegaBlog/src/components/Header/Header.jsx
+++ Episode_2/11MegaBlog/src/components/Header/Header.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
+import { Container, Logo, LogoutBtn } from '../container/Container'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 export function Header(props) {
+    const authStatus = useSelector((state) => state.auth.status);
+
+    const navigate = useNavigate();
 
 
     return (
-        <>
+        <header className='py-3 shadow bg-gray-500'>
+            <Container>
+                <nav className='flex'>
+                    <div className='mr-4'>
+                        <Link to='/'>
+                            <Logo width='70px' />
 
-        </>
+                        </Link>
+                    </div>
+                    <ul className='flex ml-auto'>
+                        {navItems.map((item) =>
+                            item.active ? (
+                                <li key={item.name}>
+                                    <button
+                                        onClick={() => navigate(item.slug)}
+                                        className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                                    >{item.name}</button>
+                                </li>
+                            ) : null
+                        )}
+                        {authStatus && (
+                            <li>
+                                <LogoutBtn />
+                            </li>
+                        )}
+                    </ul>
+                </nav>
+            </Container>
+        </header>
     )
 }
+
+export default Header
