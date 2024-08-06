b8fbf3fab8eef307b87da75e5c59d240f863cdc3
Rushikesh Here <rushikeshhere@gmail.com>
2024-08-06 22:50:31 +0530

    changes

--- Episode_2/11MegaBlog/src/components/container/Container.jsx
+++ Episode_2/11MegaBlog/src/components/container/Container.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export function Container(children) {
+export default function Container(children) {
 
 
     return <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>
