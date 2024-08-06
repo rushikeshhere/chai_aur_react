9fedc4dee6dadc90c1dadea9e3e4fdfef5691666
Rushikesh Here <rushikeshhere@gmail.com>
2024-08-06 22:49:09 +0530

    added logo.jsx in Index.js

--- Episode_2/11MegaBlog/src/components/index.js
+++ Episode_2/11MegaBlog/src/components/index.js
@@ -2,6 +2,7 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
 
 export {
     Header,
