d6bcd61118f15e85f2ae57177e50444d347d46c5
Rushikesh Here <rushikeshhere@gmail.com>
2024-08-06 22:26:01 +0530

    changes

--- Episode_2/11MegaBlog/src/components/Header/Header.jsx
+++ Episode_2/11MegaBlog/src/components/Header/Header.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
-import { Container, Logo, LogoutBtn } from '../container/Container'
+import { Container, Logo, LogoutBtn } from "../index"
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+
 
 export function Header(props) {
     const authStatus = useSelector((state) => state.auth.status);
 
     const navigate = useNavigate();
+
+    const navItems = [
+        {
+            name: 'Home',
+            slug: "/",
+            active: true
+        }, {
+            name: "Login",
+            slug: "/login",
+            active: !authStatus,
+        },
+        {
+            name: "Signup",
+            slug: "/signup",
+            active: !authStatus,
+        },
+        {
+            name: "All Posts",
+            slug: "/all-posts",
+            active: authStatus,
+        },
+        {
+            name: "Add Post",
+            slug: "/add-post",
+            active: authStatus,
+        },
+    ]
 
 
     return (
